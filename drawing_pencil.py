857cb1bc72f41cc5cc771a750b4329958e016e06
JIVESH <bravojivesh@gmail.com>
2023-12-30 23:35:37 +0530

    first stage

---
+++ drawing_pencil.py
@@ -0,0 +1,43 @@
+# def jk():
+#     return "Jose"
+#
+# var=jk()
+# var2=jk
+#
+# print(var)
+# print (var2)
+# ================================
+
+import turtle as tu
+
+def forwards():
+    biggie.forward(10)
+
+def backwards():
+    biggie.backward(10)
+
+def clockwise():
+    biggie.right(5)
+
+def counter_clockwise():
+    biggie.left(10)
+
+def cc():
+    biggie.clear()
+    biggie.penup()
+    biggie.home()
+    biggie.pendown()
+
+biggie=tu.Turtle()
+screen=tu.Screen()
+
+screen.listen()
+screen.onkey(fun=forwards,key="w")
+screen.onkey(fun=backwards,key="s")
+screen.onkey(fun=clockwise,key="a")
+screen.onkey(fun=counter_clockwise,key="d")
+screen.onkey(fun=cc,key="c")
+
+
+screen.exitonclick()
+
---
+++ main.py
@@ -0,0 +1,8 @@
+
+# set the window dimensions
+# prompt display-- go through the doc
+# create multiple instances.
+# compare the entered word with the color of the turtle. So that whether the
+# guess is correct can be determined.
+# random steps ahead until edge of the box.
+# how to know edge of the box?
